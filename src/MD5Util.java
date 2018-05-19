0cc9f6197a4c92cd4c52cd4f1ac88fe15eacd09c
Tekin Tian <tekintian@gmail.com>
2018-05-19 12:50:55 +0800

    add md5 and rsa

---
+++ src/MD5Util.java
@@ -0,0 +1,51 @@
+import java.security.MessageDigest;
+
+public class MD5Util {
+    public static void main(String[] args) {
+
+        System.out.println(MD5("123"));
+    }
+    /**
+     * 将字符串加密为MD5的结果
+     * @param source 源字符串
+     * @return 加密后的字符串
+     */
+    public static String MD5(String source) {
+        try {
+            // 对密码进行ＭD5加密处理
+            byte byteEncrypt[] = null;
+            byteEncrypt = source.getBytes("UTF8");
+            MessageDigest mdInstance = MessageDigest.getInstance("MD5");
+            mdInstance.update(byteEncrypt);
+            byte byteArray[] = mdInstance.digest();
+
+            // MD5转换编码
+            StringBuffer md5StrBuff = new StringBuffer();
+            for (int i = 0; i < byteArray.length; i++) {
+
+              //错误的方式；0xFF & byteArray[i]).length() == 1 的时候会缺少 0 位数不对
+                md5StrBuff.append(Integer.toHexString(0xff & byteArray[i]));
+
+                //正确的方式
+                /*
+                if (Integer.toHexString(0xFF & byteArray[i]).length() == 1) {
+                    md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+                } else {
+                    md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+                }
+                */
+                //end
+
+            }
+
+            return md5StrBuff.toString();
+        } catch (Exception e) {
+        }
+
+        return null;
+    }
+}
+
+
+
+
---
+++ src/RSAUtil.java
@@ -0,0 +1,118 @@
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.RSAPublicKeySpec;
+import javax.crypto.Cipher;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class RSAUtil {
+    private static char[] HEXCHAR = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+    /**
+     * main test
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println(getPublicKey());
+    }
+
+    /**
+     * 加密
+     *
+     * @param message
+     * @return
+     */
+    public static String encrypt(String message) {
+        byte[] result = null;
+        try {
+            result = encrypt(message, getPublicKey());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return toHexString(result);
+    }
+    /**
+     * 解密
+     *
+     * @param message
+     * @return
+     */
+    public static String decrypt(String message) {
+        byte[] result = null;
+        try {
+            result = decrypt(message, getPublicKey());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(result);
+    }
+    /**
+     * 加密(公钥加密、私钥加密)
+     *
+     * @param message 待加密的消息
+     * @param key 公钥或私钥
+     * @return
+     * @throws Exception
+     */
+    private static byte[] encrypt(String message, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA", new BouncyCastleProvider());
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        // 注意中文的处理
+        return cipher.doFinal(message.getBytes("utf-8"));
+    }
+    /**
+     * 解密(如果公钥加密，则用私钥解密；如果私钥加密，则用公钥解密)
+     *
+     * @param message 待解密的消息
+     * @param key 公钥或私钥
+     * @return
+     * @throws Exception
+     */
+    private static byte[] decrypt(String message, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA", new BouncyCastleProvider());
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(toBytes(message));
+    }
+    /**
+     * 通过模长和公钥指数获取公钥
+     *
+     * @param modulus 模长
+     * @param publicExponent 公钥指数
+     * @return
+     * @throws Exception
+     */
+    public static PublicKey getPublicKey() {
+        PublicKey publicKey = null;
+        String modulus = "140865665237544398577638791993321201143991791099370252934699963963887058026979531275917645451893685346013654333931757603593193739776986525943697469996693704995753266331593233395038088698299308180612215713544577462613426793519824197226393059683065343801412208205295045502348474411031999124137863144916358656019";
+        String publicExponent = "65537";
+        BigInteger m = new BigInteger(modulus);
+        BigInteger e = new BigInteger(publicExponent);
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(m, e);
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA", new BouncyCastleProvider());
+            publicKey = keyFactory.generatePublic(keySpec);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return publicKey;
+    }
+    private static final byte[] toBytes(String s) {
+        byte[] bytes;
+        bytes = new byte[s.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(s.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+    public static String toHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            sb.append(HEXCHAR[(b[i] & 0xf0) >>> 4]);
+            sb.append(HEXCHAR[b[i] & 0x0f]);
+        }
+        return sb.toString();
+    }
+
+}
